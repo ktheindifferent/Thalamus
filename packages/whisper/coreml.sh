5167613e9a2b73eb852142c8772d43dd3b349e93
Caleb Smith <caleb_smith@emiimaging.com>
2023-06-06 21:43:27 -0400

    ...

--- packages/whisper/coreml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 
-# conda create -n py310-whisper python=3.10 -y
-
-sudo -u $USER bash -c 'conda init "$(basename "${SHELL}")";conda create -n py310-whisper python=3.10 -y;conda activate py310-whisper;pip install ane_transformers;pip install openai-whisper;pip install coremltools;python3 -m pip install urllib3==1.26.6;/opt/thalamus/models/generate-coreml-model.sh tiny;/opt/thalamus/models/generate-coreml-model.sh base;/opt/thalamus/models/generate-coreml-model.sh medium;/opt/thalamus/models/generate-coreml-model.sh large;'
-
-
-
-
